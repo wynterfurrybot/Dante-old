1a78d4b8f159dde922280da12b21f8f5203d7a36
DreamerNetwork <mcpchalie@gmail.com>
2018-08-23 23:24:41 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const config = require ('./token.js');
+const token = config.token;
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(token);
