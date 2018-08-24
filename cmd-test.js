8f82da35bb64675d8086b4535c7aa6cf777f504f
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-24 01:46:54 +0100

    fixed formatting

--- cmd-test.js
+++ cmd-test.js
@@ -4,7 +4,7 @@ function debug(x) {
 }
 
 function hug(x){
-    x.msg.channel.send("<@" + x.msg.author.id + "> *walks upto *" + x.args + "* sneakily, when they aren't looking, pouncing them from behind and trapping them in a big hug!*");
+    x.msg.channel.send("<@" + x.msg.author.id + "> *walks upto*" + x.args + "*sneakily, when they aren't looking, pouncing them from behind and trapping them in a big hug!*");
 }
 
 function addCmds(x) {
