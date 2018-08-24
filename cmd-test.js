86fe44fd4a2a41c57fb385c9ee070eb0f6f0cd0b
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-24 01:30:03 +0100

    Added hug

--- cmd-test.js
+++ cmd-test.js
@@ -3,8 +3,13 @@ function debug(x) {
     x.msg.reply(x.args);
 }
 
+function hug(x){
+x.msg.send(x.args);
+}
+
 function addCmds(x) {
     x['debug'] = debug;
+    x['hug'] = hug;
 }
 
 module.exports = {
