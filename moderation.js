6c1771d357543804fcaca9e4bf93a35c88542252
Nathan Wood <nathanwoodformal@gmail.com>
2018-08-26 23:03:17 +0100

    made your code legible

--- moderation.js
+++ moderation.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js');
 
-function warn(x){
+function warn(x) {
   var usr = x.msg.mentions.users.array()[0];
 
   try {
     x.database.query("SELECT * FROM guilds WHERE guild_id = '" + x.msg.guild.id + "'", function(err, result, fields) {
 
-      if(err)
-      {
+      if (err) {
         x.log(err);
       }
 
@@ -26,15 +25,15 @@ function warn(x){
       }
 
       var embed = new Discord.RichEmbed()
-      .setTitle("Case #" + caseid)
-      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-      /*
-      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-      */
-      .setColor('#FF0000')
-      .setDescription('**A new case has been created!** \n\nType: __**WARNING**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
-      .setFooter('User Warned | Dantè Beta')
-      .setTimestamp();
+        .setTitle("Case #" + caseid)
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor('#FF0000')
+        .setDescription('**A new case has been created!** \n\nType: __**WARNING**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
+        .setFooter('User Warned | Dantè Beta')
+        .setTimestamp();
 
       try {
         x.msg.guild.channels.get(result[0].caselogs).sendMessage({
@@ -46,20 +45,16 @@ function warn(x){
 
       usr.send("You have been warned on " + x.msg.guild.name + " for the following reason: \n\n" + reason);
 
-      x.database.query("INSERT INTO `cases` (`caseref`, `serverid`, `userid`, `modid`, `reason`, `type`) VALUES (\"" + caseid + "\", \"" + x.msg.guild.id + "\", \"" + usr.id +"\", \"" + x.msg.author.id + "\", \"" + reason + "\", \"WARNING\")", function(err, result, fields) {
+      x.database.query("INSERT INTO `cases` (`caseref`, `serverid`, `userid`, `modid`, `reason`, `type`) VALUES (\"" + caseid + "\", \"" + x.msg.guild.id + "\", \"" + usr.id + "\", \"" + x.msg.author.id + "\", \"" + reason + "\", \"WARNING\")", function(err, result, fields) {
 
-        if(err)
-        {
+        if (err) {
           x.log(err);
         }
 
 
       })
     })
-  }
-
-  catch(err)
-  {
+  } catch (err) {
     x.log('ERROR - warn - ' + err);
   }
 }
@@ -71,8 +66,7 @@ function kick(x) {
   try {
     x.database.query("SELECT * FROM guilds WHERE guild_id = '" + x.msg.guild.id + "'", function(err, result, fields) {
 
-      if(err)
-      {
+      if (err) {
         x.log(err);
       }
 
@@ -91,15 +85,15 @@ function kick(x) {
       }
 
       var embed = new Discord.RichEmbed()
-      .setTitle("Case #" + caseid)
-      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-      /*
-      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-      */
-      .setColor('#FF0000')
-      .setDescription('**A new case has been created!** \n\nType: __**KICK**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
-      .setFooter('User Kicked | Dantè Beta')
-      .setTimestamp();
+        .setTitle("Case #" + caseid)
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor('#FF0000')
+        .setDescription('**A new case has been created!** \n\nType: __**KICK**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
+        .setFooter('User Kicked | Dantè Beta')
+        .setTimestamp();
 
       try {
         x.msg.guild.channels.get(result[0].caselogs).sendMessage({
@@ -111,33 +105,28 @@ function kick(x) {
       member.kick();
       usr.send("You have been kicked from " + x.msg.guild.name + " for the following reason: \n\n" + reason);
 
-      x.database.query("INSERT INTO `cases` (`caseref`, `serverid`, `userid`, `modid`, `reason`, `type`) VALUES (\"" + caseid + "\", \"" + x.msg.guild.id + "\", \"" + usr.id +"\", \"" + x.msg.author.id + "\", \"" + reason + "\", \"KICK\")", function(err, result, fields) {
+      x.database.query("INSERT INTO `cases` (`caseref`, `serverid`, `userid`, `modid`, `reason`, `type`) VALUES (\"" + caseid + "\", \"" + x.msg.guild.id + "\", \"" + usr.id + "\", \"" + x.msg.author.id + "\", \"" + reason + "\", \"KICK\")", function(err, result, fields) {
 
-        if(err)
-        {
+        if (err) {
           x.log(err);
         }
 
 
       })
     })
-  }
-
-  catch(err)
-  {
+  } catch (err) {
     x.log('ERROR - kick - ' + err);
   }
 }
 
-function ban(x){
+function ban(x) {
   var usr = x.msg.mentions.users.array()[0];
   var member = x.msg.mentions.members.array()[0];
 
   try {
     x.database.query("SELECT * FROM guilds WHERE guild_id = '" + x.msg.guild.id + "'", function(err, result, fields) {
 
-      if(err)
-      {
+      if (err) {
         x.log(err);
       }
 
@@ -156,15 +145,15 @@ function ban(x){
       }
 
       var embed = new Discord.RichEmbed()
-      .setTitle("Case #" + caseid)
-      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-      /*
-      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-      */
-      .setColor('#FF0000')
-      .setDescription('**A new case has been created!** \n\nType: __**BAN**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
-      .setFooter('User Banned | Dantè Beta')
-      .setTimestamp();
+        .setTitle("Case #" + caseid)
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor('#FF0000')
+        .setDescription('**A new case has been created!** \n\nType: __**BAN**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
+        .setFooter('User Banned | Dantè Beta')
+        .setTimestamp();
 
       try {
         x.msg.guild.channels.get(result[0].caselogs).sendMessage({
@@ -176,58 +165,46 @@ function ban(x){
       member.kick();
       usr.send("You have been banned from " + x.msg.guild.name + " for the following reason: \n\n" + reason);
 
-      x.database.query("INSERT INTO `cases` (`caseref`, `serverid`, `userid`, `modid`, `reason`, `type`) VALUES (\"" + caseid + "\", \"" + x.msg.guild.id + "\", \"" + usr.id +"\", \"" + x.msg.author.id + "\", \"" + reason + "\", \"BAN\")", function(err, result, fields) {
-
-        if(err)
-        {
+      x.database.query("INSERT INTO `cases` (`caseref`, `serverid`, `userid`, `modid`, `reason`, `type`) VALUES (\"" + caseid + "\", \"" + x.msg.guild.id + "\", \"" + usr.id + "\", \"" + x.msg.author.id + "\", \"" + reason + "\", \"BAN\")", function(err, result, fields) {
+        if (err) {
           x.log(err);
         }
-
-
       })
     })
-  }
-
-  catch(err)
-  {
+  } catch (err) {
     x.log('ERROR - ban - ' + err);
   }
 }
 
-function user(x){
+function user(x) {
   var usr = x.msg.mentions.members.array()[0];
   var punishmentinfo = "";
 
   x.database.query("SELECT * FROM `cases` WHERE userid = '" + usr.id + "'", function(err, result, fields) {
-
-    if(err)
-    {
+    if (err) {
       x.log(err);
-    }
+    } else {
 
-    else{
-
-      result.forEach(function (data, pos, array) {
-
+      result.forEach(function(data, pos, array) {
         punishmentinfo = punishmentinfo + "type: " + data.type + "\nreason: " + data.reason + "\nref: " + data.caseref + "\n\n";
-
       })
 
       var embed = new Discord.RichEmbed()
-      .setTitle(usr.user.username)
-      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-      /*
-      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-      */
-      .setColor('#FF0000')
-      .setDescription('Username: ' + usr.user.username + "#" + usr.user.discriminator + '\nID: ' + usr.id + '\nJoined discord: ' + usr.user.createdAt + "\nJoined server: " + usr.joinedAt + "\n\n__Punishments__\n" + punishmentinfo)
-      .setFooter('User Info | Dantè Beta')
-      .setTimestamp();
+        .setTitle(usr.user.username)
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor('#FF0000')
+        .setDescription('Username: ' + usr.user.username + "#" + usr.user.discriminator + '\nID: ' + usr.id + '\nJoined discord: ' + usr.user.createdAt + "\nJoined server: " + usr.joinedAt + "\n\n__Punishments__\n" + punishmentinfo)
+        .setFooter('User Info | Dantè Beta')
+        .setTimestamp();
 
-      x.msg.channel.send({embed});
+      x.msg.channel.send({
+        embed
+      });
     }
-
-  })
+  });
 }
 
 
