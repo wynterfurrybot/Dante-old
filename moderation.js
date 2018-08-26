238310c0d5331b291ee2d443e50e7b7d14f37e32
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-26 22:09:12 +0100

    overhauled moderation, indented too

--- moderation.js
+++ moderation.js
@@ -33,7 +33,7 @@ function warn(x){
       */
       .setColor('#FF0000')
       .setDescription('**A new case has been created!** \n\nType: __**WARNING**__ \nReason: **' + reason + '** \nUser: <@' + usr.id + '>')
-      .setFooter('User Warned | Damien Beta')
+      .setFooter('User Warned | Dantè Beta')
       .setTimestamp();
 
       try {
@@ -45,6 +45,16 @@ function warn(x){
       }
 
     usr.send("You have been warned on " + x.msg.guild.name + " for the following reason: \n\n" + reason);
+
+    x.database.query("INSERT INTO `guilds` (caseref, serverid, userid, modid, reason, type) VALUES ('" + caseid + "', '" + x.msg.guild.id + "', '" + usr.id +"', ' " + x.msg.author.id + "', '" + reason + "', 'WARNING')", function(err, result, fields) {
+
+      if(err)
+      {
+        x.log(err);
+      }
+
+
+    })
     })
   }
 
@@ -54,8 +64,47 @@ function warn(x){
   }
 }
 
+function user(x){
+  var usr = x.msg.mentions.members.array()[0];
+  var punishmentinfo = "";
+
+    x.database.query("SELECT * FROM cases WHERE userid = '" + usr.id + "'", function(err, result, fields) {
+
+if(err)
+{
+  x.log(err);
+}
+
+else{
+
+  result.forEach(function (data, pos, array) {
+
+          punishmentinfo = punishmentinfo + "type: " + data.type + "\nreason: " + data.reason + "\nref: " data.caseref + "\n\n";
+
+        })
+
+        var embed = new Discord.RichEmbed()
+        .setTitle(usr.username)
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+        */
+        .setColor('#FF0000')
+        .setDescription('Username: ' + usr.user.username + "#" + usr.user.discriminator + '\nID: ' + usr.id + '\nJoined discord: ' + usr.user.createdAt + "\nJoined server: " + usr.user.joinedAt + "\n\n__Punishments__\n" + punishmentinfo)
+        .setFooter('User Info | Dantè Beta')
+        .setTimestamp();
+
+x.msg.channel.send({embed});
+}
+
+    })
+}
+
+
+
 function addCmds(x) {
   x['warn'] = warn;
+  x['user'] = user;
 }
 
 
