0ab7760deff869e7b9b4d2eff2946ea3c8c8c2f8
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 22:59:35 +0100

    Test?

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -5,39 +5,31 @@ function gayyiff (x) {
   if(!x.msg.channel.nsfw) return;
   var fetch = true;
   var blockedtags = new Set(['father_and_son', 'young', 'cub']);
-  var request = e621.random("m/m", "E", 1, post => {
-  var tags = post[0]['tags'];
+  while(fetch) {
+    var request = e621.random("m/m", "E", 1, post => {
+      var tags = post[0]['tags'];
 
-    if (tags.includes(blockedtags))
-    {
+      if (tags.includes(blockedtags))
+      {
+        x.log('Got failed image -- lets try again!')
+        return;
+      }
+      else{fetch = false;}
       var embed = new Discord.RichEmbed()
-        .setTitle("Failed")
-        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-        /*
-         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-         */
-        .setColor('#FF0000')
-        .setDescription('Could not post this image.')
-        .setFooter('Image tags blocked. \nWill fix this tomorrow to generate a new image.')
-        .setTimestamp();
+      .setTitle("New yiff image -- score: " + post[0]['score'])
+      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+      /*
+      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+      */
+      .setColor('#FF0000')
+      .setDescription(post[0]['description'] + "\nSource: http://e621.net/post/show/" + post[0]['id'])
+      .setFooter('Tags: ' + post[0]['tags'])
+      .setImage(post[0]['sample_url'])
+      .setTimestamp();
 
-        x.msg.channel.send({embed});
-      return;
-    }
-  var embed = new Discord.RichEmbed()
-    .setTitle("New yiff image -- score: " + post[0]['score'])
-    .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-    /*
-     * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-     */
-    .setColor('#FF0000')
-    .setDescription(post[0]['description'] + "\nSource: http://e621.net/post/show/" + post[0]['id'])
-    .setFooter('Tags: ' + post[0]['tags'])
-    .setImage(post[0]['sample_url'])
-    .setTimestamp();
-
-    x.msg.channel.send({embed});
-})
+      x.msg.channel.send({embed});
+    })
+  }
 }
 
 
