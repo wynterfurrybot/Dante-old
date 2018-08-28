6140a6a77eb0256785a2a42743e0b0b3bd8d6c67
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 23:00:46 +0100

    Reverted.

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -5,31 +5,39 @@ function gayyiff (x) {
   if(!x.msg.channel.nsfw) return;
   var fetch = true;
   var blockedtags = new Set(['father_and_son', 'young', 'cub']);
-  while(fetch) {
-    var request = e621.random("m/m", "E", 1, post => {
-      var tags = post[0]['tags'];
+  var request = e621.random("m/m", "E", 1, post => {
+  var tags = post[0]['tags'];
 
-      if (tags.includes(blockedtags))
-      {
-        x.log('Got failed image -- lets try again!')
-        return;
-      }
-      else{fetch = false;}
+    if (tags.includes(blockedtags))
+    {
       var embed = new Discord.RichEmbed()
-      .setTitle("New yiff image -- score: " + post[0]['score'])
-      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-      /*
-      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-      */
-      .setColor('#FF0000')
-      .setDescription(post[0]['description'] + "\nSource: http://e621.net/post/show/" + post[0]['id'])
-      .setFooter('Tags: ' + post[0]['tags'])
-      .setImage(post[0]['sample_url'])
-      .setTimestamp();
+        .setTitle("Failed")
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor('#FF0000')
+        .setDescription('Could not post this image.')
+        .setFooter('Image tags blocked. \nWill fix this tomorrow to generate a new image.')
+        .setTimestamp();
 
-      x.msg.channel.send({embed});
-    })
-  }
+        x.msg.channel.send({embed});
+      return;
+    }
+  var embed = new Discord.RichEmbed()
+    .setTitle("New yiff image -- score: " + post[0]['score'])
+    .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+    /*
+     * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+     */
+    .setColor('#FF0000')
+    .setDescription(post[0]['description'] + "\nSource: http://e621.net/post/show/" + post[0]['id'])
+    .setFooter('Tags: ' + post[0]['tags'])
+    .setImage(post[0]['sample_url'])
+    .setTimestamp();
+
+    x.msg.channel.send({embed});
+})
 }
 
 
