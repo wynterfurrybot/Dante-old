213554fd162880a519a2e1efcd7855ba9a7e713b
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-28 21:47:30 +0100

    Tiredness doesn't help at all.

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -6,7 +6,7 @@ function gayyiff (x) {
   var request = e621.random("m/m", "E", 1, body => {
   var post = JSON.parse(body);
   var embed = new Discord.RichEmbed()
-    .setTitle("Case #" + caseid)
+    .setTitle("New yiff image -- score: " + post[0]['score'])
     .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
     /*
      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
