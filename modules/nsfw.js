c60db0217878974479cb40f2c2a8e1facfc464c8
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-29 17:21:34 +0100

    fuck it, I can't get limits to work.

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -3,16 +3,7 @@ const Discord = require('discord.js');
 
 function e6 (x) {
   if(!x.msg.channel.nsfw) return;
-  var params = x.args.split(' ');
-  var tags = params[0];
-  var limit = params[1];
-  if(!limit)
-  {
-    limit = 1;
-  }
-
-x.log('limit:' + limit);
-  var request = e621.random(tags, "E", limit, post => {
+  var request = e621.random(x.args, "E", 1, post => {
     var embed = new Discord.RichEmbed()
     .setTitle("New yiff image -- score: " + post[0]['score'])
     .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
@@ -26,46 +17,8 @@ x.log('limit:' + limit);
     .setTimestamp();
 
     x.msg.channel.send({embed});
-
-    // Stfu I'm lazy and couldn't be bothered to use a foreach set or something else.
-      if (limit === 2)
-      {
-        var embed = new Discord.RichEmbed()
-        .setTitle("New yiff image -- score: " + post[1]['score'])
-        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-        /*
-        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        */
-        .setColor('#FF0000')
-        .setDescription(post[1]['description'] + "\nSource: http://e621.net/post/show/" + post[1]['id'])
-        .setFooter('Tags: ' + post[1]['tags'])
-        .setImage(post[1]['sample_url'])
-        .setTimestamp();
-
-        x.msg.channel.send({embed});
-      }
-
-      if (limit === 3)
-      {
-        var embed = new Discord.RichEmbed()
-        .setTitle("New yiff image -- score: " + post[2]['score'])
-        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-        /*
-        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        */
-        .setColor('#FF0000')
-        .setDescription(post[2]['description'] + "\nSource: http://e621.net/post/show/" + post[2]['id'])
-        .setFooter('Tags: ' + post[2]['tags'])
-        .setImage(post[2]['sample_url'])
-        .setTimestamp();
-
-        x.msg.channel.send({embed});
-      }
   })
 }
-
-
-
 
 function addCmds(x) {
   x["e6"] = e6;
