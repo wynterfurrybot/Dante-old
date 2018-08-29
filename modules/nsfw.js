85157d0b218503005174a40953863519ca7c5bc3
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-29 17:09:12 +0100

    limit up to 3.

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -25,8 +25,45 @@ function e6 (x) {
     .setTimestamp();
 
     x.msg.channel.send({embed});
+
+    // Stfu I'm lazy and couldn't be bothered to use a foreach set or something else.
+      if (limit === 2)
+      {
+        var embed = new Discord.RichEmbed()
+        .setTitle("New yiff image -- score: " + post[0]['score'])
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+        */
+        .setColor('#FF0000')
+        .setDescription(post[1]['description'] + "\nSource: http://e621.net/post/show/" + post[1]['id'])
+        .setFooter('Tags: ' + post[1]['tags'])
+        .setImage(post[1]['sample_url'])
+        .setTimestamp();
+
+        x.msg.channel.send({embed});
+      }
+
+      if (limit === 3)
+      {
+        var embed = new Discord.RichEmbed()
+        .setTitle("New yiff image -- score: " + post[0]['score'])
+        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+        /*
+        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+        */
+        .setColor('#FF0000')
+        .setDescription(post[2]['description'] + "\nSource: http://e621.net/post/show/" + post[2]['id'])
+        .setFooter('Tags: ' + post[2]['tags'])
+        .setImage(post[2]['sample_url'])
+        .setTimestamp();
+
+        x.msg.channel.send({embed});
+      }
   })
-}
+
+
+
 
 
 function addCmds(x) {
