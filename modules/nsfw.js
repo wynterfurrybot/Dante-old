3fab3661d219220f963488510cc3e8e812a529e3
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-29 11:56:11 +0100

    Attempt #1001 at regenerating after blocked.

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -6,43 +6,37 @@ function gayyiff (x) {
   var fetch = true;
   var blocked = false;
   var blockedtags = new Set(['father_and_son', 'young', 'cub', 'bestiality', 'human', 'r34']);
-  var request = e621.random("m/m", "E", 1, post => {
-  var tags = post[0]['tags'];
-  blockedtags.forEach(function (tag){
-    if (tags.includes(tag)){
-      blocked = true;
-    }
-  })
-    if (blocked)
-    {
+
+  do{
+    var request = e621.random("m/m", "E", 1, post => {
+      var tags = post[0]['tags'];
+      blockedtags.forEach(function (tag){
+        if (tags.includes(tag)){
+          blocked = true;
+        }
+      })
+      if (blocked)
+      {
+        x.log('got blocked tag... will the retry work?')
+        return;
+      }
+      fetch = false;
       var embed = new Discord.RichEmbed()
-        .setTitle("Failed")
-        .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-        /*
-         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-         */
-        .setColor('#FF0000')
-        .setDescription('Could not post this image.')
-        .setFooter('Image tags blocked. \nWill fix this tomorrow to generate a new image.')
-        .setTimestamp();
+      .setTitle("New yiff image -- score: " + post[0]['score'])
+      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+      /*
+      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+      */
+      .setColor('#FF0000')
+      .setDescription(post[0]['description'] + "\nSource: http://e621.net/post/show/" + post[0]['id'])
+      .setFooter('Tags: ' + post[0]['tags'])
+      .setImage(post[0]['sample_url'])
+      .setTimestamp();
 
-        x.msg.channel.send({embed});
-      return;
-    }
-  var embed = new Discord.RichEmbed()
-    .setTitle("New yiff image -- score: " + post[0]['score'])
-    .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-    /*
-     * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-     */
-    .setColor('#FF0000')
-    .setDescription(post[0]['description'] + "\nSource: http://e621.net/post/show/" + post[0]['id'])
-    .setFooter('Tags: ' + post[0]['tags'])
-    .setImage(post[0]['sample_url'])
-    .setTimestamp();
-
-    x.msg.channel.send({embed});
-})
+      x.msg.channel.send({embed});
+    })
+  }
+  while (fetch);
 }
 
 
