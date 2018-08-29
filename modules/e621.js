254ad142cfcb0799ac2fc91488c7342e585f1cb1
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-29 18:29:07 +0100

    Just realised I had the user agent set to another person's username XD

--- modules/e621.js
+++ modules/e621.js
@@ -12,7 +12,7 @@ function random(tags, rating, limit, callback) {
     "uri": "https://e621.net/post/index.json?tags=" + tags + "%20order:random+rating:" + rating +"&limit=" + limit,
     "followRedirect":false,
     "headers": {
-      'User-Agent': 'E621APIWrapper/1.0 (by Darkmane on e621)'
+      'User-Agent': 'E621APIWrapper/1.0 (by DarkmaneArweinydd on e621)'
     }
   },function (err, res, body) {
     var post = JSON.parse(body);
