1fced5162d02867cee195e229da5f95dbea6389e
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-29 17:59:03 +0100

    fix??

--- modules/nsfw.js
+++ modules/nsfw.js
@@ -4,20 +4,7 @@ const Discord = require('discord.js');
 function e6 (x) {
   if(!x.msg.channel.nsfw) return;
   var request = e621.random(x.args, "E", 1, post => {
-    if(!post[0]['score'])
-    {
-      var embed = new Discord.RichEmbed()
-      .setTitle("We could not get an image this time")
-      .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
-      /*
-      * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-      */
-      .setColor('#FF0000')
-      .setDescription("It has took more than 10 attempts to get your image due to our blocked tags filter. \n\nPlease try again.")
-      .setFooter('Blocked image')
-      .setTimestamp();
-        x.msg.reply({embed});
-    }
+    try{
     var embed = new Discord.RichEmbed()
     .setTitle("New yiff image -- score: " + post[0]['score'])
     .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
@@ -31,6 +18,21 @@ function e6 (x) {
     .setTimestamp();
 
     x.msg.channel.send({embed});
+  }
+
+  catch{
+    var embed = new Discord.RichEmbed()
+    .setTitle('Blocked Image')
+    .setAuthor("Dantè", "https://i.imgur.com/FUUg9dM.png")
+    /*
+    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+    */
+    .setColor('#FF0000')
+    .setDescription('Could not retrieve an image for this search.')
+    .setTimestamp();
+
+    x.msg.channel.send({embed});
+  }
   })
 }
 
