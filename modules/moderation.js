b8054b839fb9c41ea2eae82188bbad715b4daacd
Darkmane Arweinydd <mcpchalie@gmail.com>
2018-08-30 22:52:28 +0100

    I swear if this doesn't work :eyes:

--- modules/moderation.js
+++ modules/moderation.js
@@ -4,7 +4,7 @@
 const Discord = require('discord.js');
 
 function hasPermission(msg, cmd, permission) {
-  if (msg.member.hasPermission(permission)) {
+  if (msg.member.hasPermission(permission) || msg.channel.hasPermissions(msg.member).FLAGS.MANAGE_MESSAGES)) {
     console.log(`${msg.author.username} passed the permission check for ${cmd} on ${msg.channel.name}`);
     return true;
   } else {
