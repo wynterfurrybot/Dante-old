3b921d2d15c6159281e1dde683b27db58edfd3b0
jørn <40339520+joirn-the-red-fox@users.noreply.github.com>
2018-09-02 20:11:10 +0100

    rounded ping

--- modules/help.js
+++ modules/help.js
@@ -32,7 +32,7 @@ function help(x) {
 function info(x) {
   x.msg.channel.send(x.getEmbed(x.config.info, {
     "guilds": `${x.client.guilds.size}`,
-    "ping": `${x.client.ping}`
+    "ping": `${Math.round(x.client.ping)}`
   }));
 }
 
